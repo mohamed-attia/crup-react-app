c6b59be3c2701dc78b8e34b619f2dbf8c56dbbf2
mohamed-attia <m.attia@outlook.sa>
2020-03-13 21:55:14 +0300

    Updates

---
+++ precache-manifest.5bac1d8a9358c38c56fe57e589c36a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57552cee62404c23c80d6ec316292aa7",
+    "url": "/crup-react-app/index.html"
+  },
+  {
+    "revision": "3926b7d6f93b3ff2e618",
+    "url": "/crup-react-app/static/css/main.754c148b.chunk.css"
+  },
+  {
+    "revision": "03e2b191edef12cd0c78",
+    "url": "/crup-react-app/static/js/2.7dcacc75.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/crup-react-app/static/js/2.7dcacc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3926b7d6f93b3ff2e618",
+    "url": "/crup-react-app/static/js/main.860103f0.chunk.js"
+  },
+  {
+    "revision": "7d9fcc90d04389d1147f",
+    "url": "/crup-react-app/static/js/runtime-main.d3dfc237.js"
+  }
+]);
